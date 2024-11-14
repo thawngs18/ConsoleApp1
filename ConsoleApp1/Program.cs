815e2bda6164895c7ee328869048ad7fa150cce6
thawngs18 <apgpdt10@gmail.com>
2024-11-14 14:33:40 +0700

    modified message again

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello,C# Academy!");
+            Console.WriteLine("Hello,C# Academy!one more modification!");
         }
     }
 }
