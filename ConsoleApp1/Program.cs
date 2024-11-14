0725666abd6ddd3d64b2d58f3f95bb695524c372
Thắng Phan Đức <apgpdt10@gmail.com>
2024-11-14 15:26:06 +0700

    ko chui teo ko nx chua anh ba

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Dm teo konn");
+            Console.WriteLine("Dm anh ba");
         }
     }
 }
